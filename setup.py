9c6351895584f9c6f0343a1ac9611f4016cd950c
Sayantani Sensharma <sayantani.sensharma@fau.de>
2022-12-04 19:32:48 +0100

    Added required files to make repo pip installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    version='0.1.0',
+    description='A package to create different coloured snowflakes',
+    url='https://github.com/TaniSensharma/First-Repository',
+    author='Sayantani Sensharma',
+    author_email='sayantani.sensharma@fau.de',
+    packages=['snowflake'],
+    install_requires=[
+        'numpy', 'turtle'
+    ],
+)
